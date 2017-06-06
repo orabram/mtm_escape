d35d50184ab0932db024cf0242b9a045df25afa4
victorbanyas <victan98@gmail.com>
2017-06-06 21:03:18 +0300

    Partial escaperoom.c Implementation
    - A change (suggested) in create_escape_room structure - returns
    EscapeRoom instead of MtmErrorCode
    - In escaperoom.h: EscapeRoom escape >> EscapeRoom room (Just for better
    readabilitry) EXCEPT for SET functions (uniplemented)
    - Some function names in C file may differ from their names in H file

---
+++ escaperoom.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "escaperoom.h"
+
+struct escaperoom {
+    char* email;
+    int id;
+    int price;
+    int num_ppl;
+    int open_hour;
+    int close_hour;
+    int difficulty;
+    TechnionFaculty faculty;
+    Set OrdersSet;
+};
+
+static MtmErrorCode set_working_hrs(EscapeRoom room, char *hrs);
+
+EscapeRoom create_escape_room(char *email, int id, int price,
+                              int num_ppl, char *working_hrs, int difficulty) {
+    EscapeRoom room = malloc(sizeof(*room));
+    if (room == NULL)
+        return NULL;
+    if (id < 0 || price % 4 != 0 || price <= 0 || num_ppl <= 0 ||
+            difficulty < 1 || difficulty > 10) {
+        free(room);
+        return NULL;
+    }
+    room->id = id;
+    room->num_ppl = num_ppl;
+    room->difficulty = difficulty;
+    if (set_working_hrs(room, working_hrs) == MTM_INVALID_PARAMETER) {
+        free(room);
+        return NULL;
+    }
+    room->email = malloc(strlen(email) + 1);
+    if (room->email == NULL) {
+        free(room);
+        return NULL;
+    }
+    strcpy(room->email, email);
+    return room;
+}
+
+MtmErrorCode reset_escape_room(EscapeRoom room)
+{
+    free(room->email);
+    room->email = NULL;
+    room->id = 0;
+    room->price = 0;
+    room->num_ppl = 0;
+    room->open_hour = 0;
+    room->close_hour = 0;
+    room->difficulty = 0;
+    return MTM_SUCCESS;
+}
+
+MtmErrorCode escape_room_get_email(EscapeRoom room, char *email)
+{
+    assert(room->email != NULL);
+    strcpy(email, room->email);
+    return MTM_SUCCESS;
+}
+
+MtmErrorCode escape_room_get_id(EscapeRoom room, int* id)
+{
+    assert(room->id >= 0);
+    *id = room->id;
+    return MTM_SUCCESS;
+}
+MtmErrorCode get_room_price(EscapeRoom room, int *price)
+{
+    assert(room->price > 0);
+    assert(room->price % 4 == 0);
+    *price = room->price;
+    return MTM_SUCCESS;
+}
+
+MtmErrorCode get_room_num_ppl(EscapeRoom room, int *num_ppl)
+{
+    assert(room->num_ppl > 0);
+    *num_ppl = room->num_ppl;
+    return MTM_SUCCESS;
+}
+
+MtmErrorCode get_room_working_hrs(EscapeRoom room, char **working_hrs)
+{
+    assert(room->open_hour >= 0 && room->open_hour <= 24);
+    assert(room->close_hour >= 0 && room->close_hour <= 24);
+    assert(room->close_hour > room->open_hour);
+    sprintf(*working_hrs, "%d-%d", room->open_hour, room->close_hour);
+    return MTM_SUCCESS;
+}
+
+MtmErrorCode get_room_difficulty(EscapeRoom room, int *difficulty)
+{
+    assert(room->difficulty >=1 && room->difficulty <= 10);
+    *difficulty = room->difficulty;
+    return MTM_SUCCESS;
+}
+
+/**
+ * An auxiliary function for setting the working hours of the room
+ * It receives working hours in a string format and sets the opening hour and
+ * closing hour fields accordingly
+ */
+static MtmErrorCode set_working_hrs(EscapeRoom room, char *hrs)
+{
+    if (hrs == NULL)
+        return MTM_INVALID_PARAMETER;
+    int open_hrs, close_hrs;
+    sscanf(hrs, "%d-%d", open_hrs, close_hrs);
+    if (open_hrs < 0 || open_hrs > 24 || close_hrs < 0 || close_hrs > 24 ||
+        open_hrs >= close_hrs) {
+        return MTM_INVALID_PARAMETER;
+    }
+    room->open_hour = open_hrs;
+    room->close_hour = close_hrs;
+    return MTM_SUCCESS;
+}
+
--- escaperoom.h
+++ escaperoom.h
@@ -6,24 +6,14 @@
 #include "set.h"
 #include "order.h"
 
-typedef struct escaperoom{
-    char* email;
-    int id;
-    int price;
-    int num_ppl;
-    int working_hrs;
-    int difficulty;
-    TechnionFaculty faculty;
-    Set OrdersSet;
-} *EscapeRoom;
+typedef struct escaperoom *EscapeRoom;
 
 //Creates a new escape room. Resets OrdersSet.
-MtmErrorCode create_escape_room(EscapeRoom escape, char* email, int id, int price, int num_ppl,
-                                int working_hrs, int difficulty);
+EscapeRoom create_escape_room(char* email, int id, int price,
+                                int num_ppl, char *working_hrs, int difficulty);
 
 //Copies an existing escape room.
-MtmErrorCode escape_room_copy(EscapeRoom original_escape,
-                              EscapeRoom new_escape);
+MtmErrorCode escape_room_copy(EscapeRoom new_room, EscapeRoom original_room);
 
 //Switches the current corporate email with a new one.
 //NOTE: It does not change the original one.
@@ -51,44 +41,44 @@ MtmErrorCode escape_room_set_difficulty(EscapeRoom escape, int difficulty);
 MtmErrorCode escape_room_set_faculty(EscapeRoom escape, TechnionFaculty faculty);
 
 //Returns the email into the given variable.
-MtmErrorCode escape_room_get_email(EscapeRoom escape, char* email);
+MtmErrorCode escape_room_get_email(EscapeRoom room, char* email);
 
 //Returns the id into the given variable.
-MtmErrorCode escape_room_get_id(EscapeRoom escape, int* id);
+MtmErrorCode escape_room_get_id(EscapeRoom room, int* id);
 
 //Returns the price into the given variable.
-MtmErrorCode escape_room_get_price(EscapeRoom escape, int* price);
+MtmErrorCode escape_room_get_price(EscapeRoom room, int* price);
 
 //Returns the num_ppl into the given variable.
-MtmErrorCode escape_room_get_num_ppl(EscapeRoom escape, int* num_ppl);
+MtmErrorCode escape_room_get_num_ppl(EscapeRoom room, int* num_ppl);
 
 //Returns the working_hrs into the given variable.
-MtmErrorCode escape_room_get_working_hrs(EscapeRoom escape, int* working_hrs);
+MtmErrorCode escape_room_get_working_hrs(EscapeRoom room, char **working_hrs);
 
 //Returns the difficulty into the given variable.
-MtmErrorCode escape_room_get_difficulty(EscapeRoom escape, int* difficulty);
+MtmErrorCode escape_room_get_difficulty(EscapeRoom room, int* difficulty);
 
 //Returns the faculty into the given variable.
-MtmErrorCode escape_room_get_faculty(EscapeRoom escape,
-                                     TechnionFaculty* faculty);
+MtmErrorCode escape_room_get_faculty(EscapeRoom room,
+                                     TechnionFaculty *faculty);
 
 //Calculates how recommended this room is to a specific party,
 // and stores the value in the given variable.
-MtmErrorCode escape_room_calculate_recommended_value(EscapeRoom escape,
+MtmErrorCode escape_room_calculate_recommended_value(EscapeRoom room,
                                                      int skill_level,
                                                      int party_size,
                                                      int* recommended_value);
 
 //Adds a new order to the room.
-MtmErrorCode escape_room_add_order(EscapeRoom escape, Order ord);
+MtmErrorCode escape_room_add_order(EscapeRoom room, Order order);
 
 //Checks to see if two orders match each other's time.
-MtmErrorCode escape_room_colliding_times(EscapeRoom escape, Order ord);
+MtmErrorCode escape_room_colliding_times(EscapeRoom room, Order order);
 
 //Removes an order.
-MtmErrorCode escape_room_remove_order(EscapeRoom escape, Order ord);
+MtmErrorCode escape_room_remove_order(EscapeRoom room, Order order);
 
 //Destroys the room.
-MtmErrorCode escape_room_destroy(EscapeRoom escape, Order ord);
+MtmErrorCode escape_room_destroy(EscapeRoom room, Order order);
 
 #endif
